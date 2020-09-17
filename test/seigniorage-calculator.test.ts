16442f1c2c149f2dba66b4183ad5d7fb082037c9
modagi <modagi@gmail.com>
2020-09-17 13:30:16 +0900

    add calculateExpectedSeigWithCommission function

--- test/seigniorage-calculator.test.ts
+++ test/seigniorage-calculator.test.ts
@@ -1,4 +1,4 @@
-import { calculateExpectedSeig, Calculator } from '../src'
+import { calculateExpectedSeig, calculateExpectedSeigWithCommission, Calculator } from '../src'
 
 import { BN } from 'bn.js'
 const { toBN } = require('web3-utils')
@@ -6,6 +6,15 @@ const chai = require('chai')
 chai.use(require('chai-bn')(BN)).should()
 
 const RAY = toBN('1000000000000000000000000000') // 1e+27
+const tolerance = toBN('1000000000000000000')
+
+const checkNumber = (
+  a: BN,
+  b: BN
+) => {
+  const v = a.sub(b).abs();
+  v.should.be.bignumber.lte(tolerance)
+}
 
 describe('calculateExpectedSeig function', function () {
   it('should get correct value', function () {
@@ -20,6 +29,27 @@ describe('calculateExpectedSeig function', function () {
 
     const expected = toBN('8309568960000000000000000000')
     result.should.be.bignumber.equal(expected)
+  })
+})
+
+describe('calculateExpectedSeigWithCommission function', function () {
+  it('should get correct value', function () {
+    const result = calculateExpectedSeigWithCommission(
+      toBN('1'),
+      toBN('10000'),
+      toBN('5000').mul(RAY),
+      toBN('2000000').mul(RAY),
+      toBN('50000000').mul(RAY),
+      toBN('4').mul(RAY).div(toBN('10')),
+      toBN('7').mul(RAY).div(toBN('10')),
+      true,
+      toBN('500000').mul(RAY),
+      toBN('1000000').mul(RAY),
+      false
+    )
+
+    const expected = toBN('70631336160001000000000000000')
+    checkNumber(result, expected)
   })
 })
 
